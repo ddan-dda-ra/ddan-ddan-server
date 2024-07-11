c4c9dff37c947b7fa488d626a01c609626a2452f
ddingmin <ddingminyt@gmail.com>
2024-07-11 21:20:56 +0900

    chore: init

---
+++ src/main/kotlin/notbe/tmtm/ddanddanserver/DdanDdanServerApplication.kt
@@ -0,0 +1,11 @@
+package notbe.tmtm.ddanddanserver
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DdanDdanServerApplication
+
+fun main(args: Array<String>) {
+    runApplication<DdanDdanServerApplication>(*args)
+}
---
+++ src/test/kotlin/notbe/tmtm/ddanddanserver/DdanDdanServerApplicationTests.kt
@@ -0,0 +1,13 @@
+package notbe.tmtm.ddanddanserver
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DdanDdanServerApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
