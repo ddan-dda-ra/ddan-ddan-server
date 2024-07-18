08798d39b802d070f874b17b7daab164f9b8eb0b
ddingmin <ddingmin.dev@gmail.com>
2024-07-18 23:08:24 +0900

    chore: ktlint 적용 (#2)
    * chore: 의존성 추가
    
    * chore: official style 적용
    
    * refactor: ktlint 에 맞게 코드 변경
    
    * chore: 의존성 네이밍 일치화
    
    * chore: ktlint 체크 workflow 작성

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("org.springframework.boot") version "3.3.1"
     id("io.spring.dependency-management") version "1.1.5"
+    id("org.jlleitschuh.gradle.ktlint") version "12.1.1"
     kotlin("jvm") version "1.9.24"
     kotlin("plugin.spring") version "1.9.24"
 }
