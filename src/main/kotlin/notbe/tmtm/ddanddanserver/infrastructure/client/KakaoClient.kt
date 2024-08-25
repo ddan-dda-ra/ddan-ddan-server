f5d6b56acf8d12834c110df9d758eb24b8626a4b
ddingmin <ddingmin.dev@gmail.com>
2024-08-25 12:59:16 +0900

    feat: kakao 클라이언트 에러 로깅

--- src/main/kotlin/notbe/tmtm/ddanddanserver/infrastructure/client/KakaoClient.kt
+++ src/main/kotlin/notbe/tmtm/ddanddanserver/infrastructure/client/KakaoClient.kt
@@ -3,6 +3,7 @@ package notbe.tmtm.ddanddanserver.infrastructure.client
 import notbe.tmtm.ddanddanserver.domain.exception.KakaoParseError
 import notbe.tmtm.ddanddanserver.domain.exception.KakaoRestClientError
 import notbe.tmtm.ddanddanserver.infrastructure.client.dto.response.KakaoOAuthInfoResponse
+import notbe.tmtm.ddanddanserver.infrastructure.util.logger
 import org.springframework.http.HttpHeaders
 import org.springframework.http.HttpStatusCode
 import org.springframework.http.MediaType
@@ -16,16 +17,22 @@ class KakaoClient(
 ) {
     private val baseUrl: String = "https://kapi.kakao.com"
 
-    fun getOAuthInfo(token: String) =
-        runCatching {
-            restClient
-                .get()
-                .uri(URI.create("$baseUrl/v2/user/me"))
-                .header(HttpHeaders.AUTHORIZATION, "Bearer $token")
-                .accept(MediaType.APPLICATION_JSON)
-                .retrieve()
-                .onStatus(HttpStatusCode::isError) { _, response ->
-                    throw KakaoRestClientError(response.statusCode)
-                }.toEntity(KakaoOAuthInfoResponse::class.java)
-        }.getOrElse { throw KakaoParseError(it) }
+    fun getOAuthInfo(token: String): KakaoOAuthInfoResponse =
+        restClient
+            .get()
+            .uri(URI.create("$baseUrl/v2/user/me"))
+            .header(HttpHeaders.AUTHORIZATION, "Bearer $token")
+            .accept(MediaType.APPLICATION_JSON)
+            .retrieve()
+            .onStatus(HttpStatusCode::isError) { _, response ->
+                logger().error(
+                    "Kakao API error: ${
+                        response.body.use {
+                            it.readAllBytes().toString(Charsets.UTF_8)
+                        }
+                    }",
+                )
+                throw KakaoRestClientError(response.statusCode)
+            }.toEntity(KakaoOAuthInfoResponse::class.java)
+            .body ?: throw KakaoParseError()
 }
