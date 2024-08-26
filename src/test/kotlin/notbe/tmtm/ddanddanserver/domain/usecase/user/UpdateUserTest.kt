da18fac5f33e26a53bfc0b291dcfe1a731415b20
ddingmin <ddingmin.dev@gmail.com>
2024-08-26 09:24:53 +0900

    test: User Update(PUT) 로직 테스트 코드 작성

---
+++ src/test/kotlin/notbe/tmtm/ddanddanserver/domain/usecase/user/UpdateUserTest.kt
@@ -0,0 +1,68 @@
+package notbe.tmtm.ddanddanserver.domain.usecase.user
+
+import io.mockk.every
+import io.mockk.mockk
+import jakarta.persistence.EntityNotFoundException
+import notbe.tmtm.ddanddanserver.domain.gateway.UserGateway
+import notbe.tmtm.ddanddanserver.domain.model.User
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.assertThrows
+import kotlin.test.Test
+
+class UpdateUserTest {
+    private val userGateway: UserGateway = mockk<UserGateway>()
+
+    private val updateUser: UpdateUser = UpdateUser(userGateway)
+
+    @Test
+    fun `유저 정보 수정에 성공한다`() {
+        // given
+        val user =
+            User(
+                id = "ABCDEF1234567",
+                name = "oldUserName",
+                purposeCalorie = 100,
+            )
+
+        every { userGateway.getById(user.id) } returns user
+        every { userGateway.update(user) } returns user
+
+        // when
+        val result =
+            updateUser.execute(
+                UpdateUser.UpdateUserInput(
+                    userId = "ABCDEF1234567",
+                    name = "newUserName",
+                    purposeCalorie = 200,
+                ),
+            )
+
+        // then
+        assertEquals("newUserName", result.user.name)
+        assertEquals(200, result.user.purposeCalorie)
+    }
+
+    @Test
+    fun `userId가 존재하지 않는 경우 EntityNotFoundException가 던져진다`() {
+        // given
+        val user =
+            User(
+                id = "ABCDEF1234567",
+                name = "oldUserName",
+                purposeCalorie = 100,
+            )
+
+        every { userGateway.getById(user.id) } throws EntityNotFoundException()
+
+        // when & then
+        assertThrows<EntityNotFoundException> {
+            updateUser.execute(
+                UpdateUser.UpdateUserInput(
+                    userId = "ABCDEF1234567",
+                    name = "newUserName",
+                    purposeCalorie = 200,
+                ),
+            )
+        }
+    }
+}
