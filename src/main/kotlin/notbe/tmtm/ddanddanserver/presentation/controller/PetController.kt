25449296e045c5edf697725c092f0edeb83984bc
ddingmin <ddingmin.dev@gmail.com>
2024-09-17 00:42:42 +0900

    feat: 랜덤 펫 추가 기능 구현 (#38)
    * feat: 랜덤 펫 추가 기능 구현
    
    * refactor: 와일드카드 import를 하지 않도록 변경

--- src/main/kotlin/notbe/tmtm/ddanddanserver/presentation/controller/PetController.kt
+++ src/main/kotlin/notbe/tmtm/ddanddanserver/presentation/controller/PetController.kt
@@ -3,19 +3,25 @@ package notbe.tmtm.ddanddanserver.presentation.controller
 import io.swagger.v3.oas.annotations.Operation
 import io.swagger.v3.oas.annotations.tags.Tag
 import notbe.tmtm.ddanddanserver.domain.usecase.pet.AddPet
+import notbe.tmtm.ddanddanserver.domain.usecase.pet.AddRandomPet
 import notbe.tmtm.ddanddanserver.domain.usecase.pet.GetPets
 import notbe.tmtm.ddanddanserver.presentation.dto.request.AddPetRequest
 import notbe.tmtm.ddanddanserver.presentation.dto.response.PetResponse
 import notbe.tmtm.ddanddanserver.presentation.dto.response.PetsResponse
 import org.springframework.security.core.Authentication
-import org.springframework.web.bind.annotation.*
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/v1/pets")
 @Tag(name = "펫")
 class PetController(
-    private val getPets: GetPets,
     private val addPet: AddPet,
+    private val addRandomPet: AddRandomPet,
+    private val getPets: GetPets,
 ) {
     @PostMapping("/me")
     @Operation(summary = "펫 추가", description = "펫을 추가합니다.")
@@ -28,6 +34,18 @@ class PetController(
                 AddPet.Input(
                     ownerUserId = authentication.name,
                     petType = request.petType,
+                ),
+            )
+        return PetResponse.fromDomain(result.pet)
+    }
+
+    @PostMapping("/me/random")
+    @Operation(summary = "랜덤 펫 추가", description = "랜덤으로 펫을 추가합니다.")
+    fun addRandomPet(authentication: Authentication): PetResponse {
+        val result =
+            addRandomPet.execute(
+                AddRandomPet.Input(
+                    ownerUserId = authentication.name,
                 ),
             )
         return PetResponse.fromDomain(result.pet)
