a0e03cd6bec31fa6012abb30d54cd3928715c735
ddingmin <ddingmin.dev@gmail.com>
2024-09-16 18:36:51 +0900

    feat: 펫 추가 기능 구현

---
+++ src/main/kotlin/notbe/tmtm/ddanddanserver/domain/usecase/pet/AddPet.kt
@@ -0,0 +1,31 @@
+package notbe.tmtm.ddanddanserver.domain.usecase.pet
+
+import notbe.tmtm.ddanddanserver.domain.gateway.PetGateway
+import notbe.tmtm.ddanddanserver.domain.model.pet.Pet
+import notbe.tmtm.ddanddanserver.domain.model.pet.PetType
+import notbe.tmtm.ddanddanserver.domain.usecase.UseCase
+import org.springframework.stereotype.Component
+
+@Component
+class AddPet(
+    private val petGateway: PetGateway,
+) : UseCase<AddPet.Input, AddPet.Output> {
+    data class Input(
+        val ownerUserId: String,
+        val petType: PetType,
+    )
+
+    data class Output(
+        val pet: Pet,
+    )
+
+    override fun execute(input: Input): Output =
+        Output(
+            petGateway.save(
+                Pet.register(
+                    type = input.petType,
+                    ownerUserId = input.ownerUserId,
+                ),
+            ),
+        )
+}
--- src/main/kotlin/notbe/tmtm/ddanddanserver/presentation/dto/response/PetResponse.kt
+++ src/main/kotlin/notbe/tmtm/ddanddanserver/presentation/dto/response/PetResponse.kt
@@ -2,13 +2,14 @@ package notbe.tmtm.ddanddanserver.presentation.dto.response
 
 import io.swagger.v3.oas.annotations.media.Schema
 import notbe.tmtm.ddanddanserver.domain.model.pet.Pet
+import notbe.tmtm.ddanddanserver.domain.model.pet.PetType
 
 @Schema(description = "펫 정보 응답 DTO")
 data class PetResponse(
     @Schema(description = "Pet 식별 ID", example = "ABCDEF1234567")
     val id: String,
     @Schema(description = "Pet 종류", example = "DOG")
-    val type: String,
+    val type: PetType,
     @Schema(description = "Pet 레벨", example = "1")
     val level: Int,
     @Schema(description = "현재 레벨에 해당하는 경험치 퍼센트", example = "15.5")
@@ -19,7 +20,7 @@ data class PetResponse(
             with(userPet) {
                 PetResponse(
                     id = id,
-                    type = type.name,
+                    type = type,
                     level = getLevel().level,
                     expPercent = getExpPercent(),
                 )
