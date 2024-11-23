5037d80c9d17e6e75f17db919477bde1ac86befa
ddingmin <ddingmin.dev@gmail.com>
2024-11-23 17:09:46 +0900

    lint 적용

--- src/main/kotlin/notbe/tmtm/ddanddanserver/DdanDdanServerApplication.kt
+++ src/main/kotlin/notbe/tmtm/ddanddanserver/DdanDdanServerApplication.kt
@@ -2,7 +2,6 @@ package notbe.tmtm.ddanddanserver
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.scheduling.annotation.EnableAsync
 
 @SpringBootApplication
 class DdanDdanServerApplication
--- src/main/kotlin/notbe/tmtm/ddanddanserver/infrastructure/client/AppleClient.kt
+++ src/main/kotlin/notbe/tmtm/ddanddanserver/infrastructure/client/AppleClient.kt
@@ -2,7 +2,8 @@ package notbe.tmtm.ddanddanserver.infrastructure.client
 
 import com.fasterxml.jackson.core.type.TypeReference
 import com.fasterxml.jackson.databind.ObjectMapper
-import io.jsonwebtoken.*
+import io.jsonwebtoken.Claims
+import io.jsonwebtoken.Jwts
 import notbe.tmtm.ddanddanserver.infrastructure.client.dto.response.AppleOAuthInfoResponse
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Component
@@ -11,19 +12,17 @@ import java.math.BigInteger
 import java.security.KeyFactory
 import java.security.PublicKey
 import java.security.spec.RSAPublicKeySpec
-import java.util.*
-
+import java.util.Base64
 
 @Component
 class AppleClient(
-    @Value("\${APPLE_CLIENT_SECRET}") clientSecret: String,
-    @Value("\${APPLE_BUNDLE_ID}") bundleId: String,
-    @Value("\${APPLE_SERVICE_ID}") serviceId: String,
-    @Value("\${APPLE_KEY_ID}") keyId: String,
+    @Value("\${app.apple.client-id}") clientSecret: String,
+//    @Value("\${APPLE_BUNDLE_ID}") bundleId: String,
+    @Value("\${app.apple.team-id}") serviceId: String,
+    @Value("\${app.apple.key-id}") keyId: String,
     private val restClient: RestClient,
     private val objectMapper: ObjectMapper,
 ) {
-
     fun getOAuthInfo(token: String): AppleOAuthInfoResponse {
         val headers = parseHeaders(token)
         val appleKeys = getAppleKeys()
@@ -32,41 +31,47 @@ class AppleClient(
         val claims = parseClaims(token, publicKey)
         return AppleOAuthInfoResponse(
             id = claims["sub"].toString(),
-            properties = AppleOAuthInfoResponse.Properties(
-                nickname = claims["email"].toString(),
-            )
+            properties =
+                AppleOAuthInfoResponse.Properties(
+                    nickname = claims["email"].toString(),
+                ),
         )
     }
 
     private fun parseHeaders(token: String): Map<String, String> {
         val encodedHeader: String =
-            token.split(TOKEN_VALUE_DELIMITER.toRegex()).dropLastWhile { it.isEmpty() }
+            token
+                .split(TOKEN_VALUE_DELIMITER.toRegex())
+                .dropLastWhile { it.isEmpty() }
                 .toTypedArray()[0]
         val decodedHeader = String(Base64.getUrlDecoder().decode(encodedHeader))
         return objectMapper.readValue(
             decodedHeader,
-            object : TypeReference<Map<String, String>>() {})
+            object : TypeReference<Map<String, String>>() {},
+        )
     }
 
-    private fun getAppleKeys(): AppleKeys {
-        return restClient.get()
+    private fun getAppleKeys(): AppleKeys =
+        restClient
+            .get()
             .uri("https://appleid.apple.com/auth/keys")
             .exchange { _, clientResponse ->
                 clientResponse.bodyTo(AppleKeys::class.java)
                     ?: throw IllegalStateException("Apple 로그인 과정중 문제 발생. public key를 조회할 수 없음. $clientResponse")
             }
-    }
 
     private fun generatePublicKey(
         tokenHeaders: Map<String, String>,
         appleKeys: AppleKeys,
     ): PublicKey {
         val publicKeys: List<Key> = appleKeys.keys
-        val publicKey: Key = publicKeys.stream()
-            .filter { key -> key.alg == tokenHeaders["alg"] }
-            .filter { key -> key.kid == tokenHeaders["kid"] }
-            .findAny()
-            .orElseThrow()
+        val publicKey: Key =
+            publicKeys
+                .stream()
+                .filter { key -> key.alg == tokenHeaders["alg"] }
+                .filter { key -> key.kid == tokenHeaders["kid"] }
+                .findAny()
+                .orElseThrow()
 
         return generatePublicKeyWithApplePublicKey(publicKey)
     }
@@ -83,25 +88,28 @@ class AppleClient(
         return keyFactory.generatePublic(publicKeySpec)
     }
 
-    private fun parseClaims(idToken: String?, publicKey: PublicKey?): Claims {
-        return Jwts.parser()
+    private fun parseClaims(
+        idToken: String?,
+        publicKey: PublicKey?,
+    ): Claims =
+        Jwts
+            .parser()
             .verifyWith(publicKey)
             .build()
             .parseEncryptedClaims(idToken)
             .payload
-    }
 
     data class AppleKeys(
-        val keys: List<Key>
+        val keys: List<Key>,
     )
 
     data class Key(
-        val kty: String,  // Key Type (e.g., "RSA")
-        val kid: String,  // Key ID
-        val use: String,  // Usage (e.g., "sig")
-        val alg: String,  // Algorithm (e.g., "RS256")
-        val n: String,    // Modulus
-        val e: String     // Exponent
+        val kty: String, // Key Type (e.g., "RSA")
+        val kid: String, // Key ID
+        val use: String, // Usage (e.g., "sig")
+        val alg: String, // Algorithm (e.g., "RS256")
+        val n: String, // Modulus
+        val e: String, // Exponent
     )
 
     private companion object {
