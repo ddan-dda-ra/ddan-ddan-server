94e7a46202eb18f63b1a78363b23aae3bb238a3e
ddingmin <ddingmin.dev@gmail.com>
2025-02-16 18:35:40 +0900

    feat: SpringBootTest 비활성화

--- src/test/kotlin/notbe/tmtm/ddanddanserver/DdanDdanServerApplicationTests.kt
+++ src/test/kotlin/notbe/tmtm/ddanddanserver/DdanDdanServerApplicationTests.kt
@@ -1,9 +1,11 @@
 package notbe.tmtm.ddanddanserver
 
+import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 
 @SpringBootTest
+@Disabled
 class DdanDdanServerApplicationTests {
     @Test
     fun contextLoads() {
