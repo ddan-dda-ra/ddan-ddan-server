aabb2043e72ebd14ea1b7717d99fe04638bdf3ac
ddingmin <ddingmin.dev@gmail.com>
2025-03-07 19:57:29 +0900

    feat: impl fcm pusher

--- src/main/kotlin/notbe/tmtm/ddanddanserver/infrastructure/config/FCMProperties.kt
+++
@@ -1,9 +0,0 @@
-package notbe.tmtm.ddanddanserver.infrastructure.config
-
-import org.springframework.boot.context.properties.ConfigurationProperties
-
-@ConfigurationProperties(prefix = "fcm")
-data class FCMProperties(
-    val projectId: String,
-    val key: String,
-)
---
+++ src/main/kotlin/notbe/tmtm/ddanddanserver/infrastructure/gateway/FcmPushGatewayImpl.kt
@@ -0,0 +1,42 @@
+package notbe.tmtm.ddanddanserver.infrastructure.gateway
+
+import com.google.firebase.messaging.FirebaseMessaging
+import com.google.firebase.messaging.Message
+import com.google.firebase.messaging.Notification
+import notbe.tmtm.ddanddanserver.domain.gateway.PushGateway
+import org.springframework.stereotype.Component
+
+@Component
+class FcmPushGatewayImpl(
+    private val fcmClient: FirebaseMessaging,
+) : PushGateway {
+    override fun send(
+        deviceToken: String,
+        content: String,
+    ) {
+        val request =
+            Message
+                .builder()
+                .setToken(deviceToken)
+                .setNotification(Notification.builder().setBody(content).build())
+                .build()
+
+        fcmClient.send(request)
+    }
+
+    override fun sendAll(
+        deviceTokens: List<String>,
+        content: String,
+    ) {
+        val requests =
+            deviceTokens.map { token ->
+                Message
+                    .builder()
+                    .setToken(token)
+                    .setNotification(Notification.builder().setBody(content).build())
+                    .build()
+            }
+
+        fcmClient.sendEach(requests)
+    }
+}
