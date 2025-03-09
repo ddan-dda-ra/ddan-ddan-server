128d044b70b71a46c0112bdf3b23ac971a76a5e1
ddingmin <ddingmin.dev@gmail.com>
2025-03-09 18:07:05 +0900

    chore: 스케쥴링 사용하도록 한다

--- src/main/kotlin/notbe/tmtm/ddanddanserver/DdanDdanServerApplication.kt
+++ src/main/kotlin/notbe/tmtm/ddanddanserver/DdanDdanServerApplication.kt
@@ -3,9 +3,11 @@ package notbe.tmtm.ddanddanserver
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.boot.runApplication
+import org.springframework.scheduling.annotation.EnableScheduling
 
 @SpringBootApplication
 @ConfigurationPropertiesScan
+@EnableScheduling
 class DdanDdanServerApplication
 
 fun main(args: Array<String>) {
