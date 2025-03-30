c2a753e5a162a08bca16cc45dce8632f8f02e39f
ddingmin <ddingmin.dev@gmail.com>
2025-03-31 00:27:48 +0900

    bug: 누락된 RoutingView 필드 추가

--- src/main/kotlin/notbe/tmtm/ddanddanserver/domain/usecase/notification/NotifyCheckCalorie.kt
+++ src/main/kotlin/notbe/tmtm/ddanddanserver/domain/usecase/notification/NotifyCheckCalorie.kt
@@ -3,6 +3,7 @@ package notbe.tmtm.ddanddanserver.domain.usecase.notification
 import notbe.tmtm.ddanddanserver.domain.gateway.PushGateway
 import notbe.tmtm.ddanddanserver.domain.gateway.UserGateway
 import notbe.tmtm.ddanddanserver.domain.model.notification.PushMessage
+import notbe.tmtm.ddanddanserver.domain.model.notification.RoutingView
 import notbe.tmtm.ddanddanserver.domain.usecase.UseCase
 import org.springframework.stereotype.Component
 import org.springframework.transaction.annotation.Transactional
@@ -19,6 +20,6 @@ class NotifyCheckCalorie(
                 .findAll()
                 .filter { it.setting.isAppPushOn }
                 .filter { it.deviceToken.isNullOrEmpty().not() }
-        pushGateway.sendAll(allUsers.map { it.deviceToken!! }, PushMessage.CHECK_CALORIE.content)
+        pushGateway.sendAll(allUsers.map { it.deviceToken!! }, PushMessage.CHECK_CALORIE.content, RoutingView.MAIN)
     }
 }
