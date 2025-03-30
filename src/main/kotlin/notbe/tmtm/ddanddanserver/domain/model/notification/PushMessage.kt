e61ee249fb9ea9583a002746ab24f3414c9b663d
ddingmin <ddingmin.dev@gmail.com>
2025-03-31 00:25:15 +0900

    feat: 주간 랭킹 1등의 칼로리 푸시 발송

--- src/main/kotlin/notbe/tmtm/ddanddanserver/domain/model/notification/PushMessage.kt
+++ src/main/kotlin/notbe/tmtm/ddanddanserver/domain/model/notification/PushMessage.kt
@@ -5,4 +5,5 @@ enum class PushMessage(
 ) {
     LONG_SLEEPING_USER("\uD83D\uDE22 펫이 기다리고 있어요!"),
     CHECK_CALORIE("\uD83D\uDD25 오늘 몇 칼로리 소모했는지 확인해 볼까요?"),
+    WEEKLY_RANKING("\uD83D\uDCC8 이번 주 칼로리 소모 1위는?"),
 }
--- src/main/kotlin/notbe/tmtm/ddanddanserver/presentation/scheduler/NotificationScheduler.kt
+++ src/main/kotlin/notbe/tmtm/ddanddanserver/presentation/scheduler/NotificationScheduler.kt
@@ -1,7 +1,11 @@
 package notbe.tmtm.ddanddanserver.presentation.scheduler
 
+import notbe.tmtm.ddanddanserver.domain.model.ranking.PeriodType
+import notbe.tmtm.ddanddanserver.domain.model.ranking.RankingCriteria
 import notbe.tmtm.ddanddanserver.domain.usecase.notification.NotifyCheckCalorie
 import notbe.tmtm.ddanddanserver.domain.usecase.notification.NotifySleepingUsers
+import notbe.tmtm.ddanddanserver.domain.usecase.notification.NotifyWeeklyRanking
+import notbe.tmtm.ddanddanserver.domain.usecase.ranking.GetTopRanking
 import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Component
 
@@ -9,6 +13,8 @@ import org.springframework.stereotype.Component
 class NotificationScheduler(
     private val notifySleepingUsers: NotifySleepingUsers,
     private val notifyCheckCalorie: NotifyCheckCalorie,
+    private val notifyWeeklyRanking: NotifyWeeklyRanking,
+    private val getTopRanking: GetTopRanking,
 ) {
     @Scheduled(cron = "0 30 20 * * *") // 매일 20:30
     fun notifySleepingUsers() {
@@ -18,5 +24,12 @@ class NotificationScheduler(
     @Scheduled(cron = "0 30 20 * * *") // 매일 20:30
     fun notifyCheckRankingMessage() {
         notifyCheckCalorie.execute(Unit)
+    }
+
+    @Scheduled(cron = "0 30 20 * * 5") // 매주 금요일 20:30
+    fun notifyWeeklyRanking() {
+        val topRanking =
+            getTopRanking.execute(GetTopRanking.Input(RankingCriteria.TOTAL_CALORIES, PeriodType.WEEKLY)).user ?: return
+        notifyWeeklyRanking.execute(topRanking.totalCalories)
     }
 }
