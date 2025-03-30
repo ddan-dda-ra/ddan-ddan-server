e216df4b276b26f12c45ccad1b1c4ba9dd822af5
ddingmin <ddingmin.dev@gmail.com>
2025-03-30 21:57:08 +0900

    feat: 칼로리 확인 푸시 알람 전송

--- src/main/kotlin/notbe/tmtm/ddanddanserver/domain/model/notification/PushMessage.kt
+++ src/main/kotlin/notbe/tmtm/ddanddanserver/domain/model/notification/PushMessage.kt
@@ -4,4 +4,5 @@ enum class PushMessage(
     val content: String,
 ) {
     LONG_SLEEPING_USER("\uD83D\uDE22 펫이 기다리고 있어요!"),
+    CHECK_CALORIE("\uD83D\uDD25 오늘 몇 칼로리 소모했는지 확인해 볼까요?"),
 }
--- src/main/kotlin/notbe/tmtm/ddanddanserver/presentation/scheduler/NotificationScheduler.kt
+++ src/main/kotlin/notbe/tmtm/ddanddanserver/presentation/scheduler/NotificationScheduler.kt
@@ -1,5 +1,6 @@
 package notbe.tmtm.ddanddanserver.presentation.scheduler
 
+import notbe.tmtm.ddanddanserver.domain.usecase.notification.NotifyCheckCalorie
 import notbe.tmtm.ddanddanserver.domain.usecase.notification.NotifySleepingUsers
 import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Component
@@ -7,9 +8,15 @@ import org.springframework.stereotype.Component
 @Component
 class NotificationScheduler(
     private val notifySleepingUsers: NotifySleepingUsers,
+    private val notifyCheckCalorie: NotifyCheckCalorie,
 ) {
     @Scheduled(cron = "0 30 20 * * *") // 매일 20:30
     fun notifySleepingUsers() {
         notifySleepingUsers.execute(Unit)
+    }
+
+    @Scheduled(cron = "0 30 20 * * *") // 매일 20:30
+    fun notifyCheckRankingMessage() {
+        notifyCheckCalorie.execute(Unit)
     }
 }
