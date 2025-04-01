3ff3a895c1282b6b65908732372afab03b306455
ddingmin <ddingmin.dev@gmail.com>
2025-04-01 23:30:58 +0900

    feat: 로깅 필터를 추가해 로그를 남긴다 (#133)

---
+++ src/main/kotlin/notbe/tmtm/ddanddanserver/presentation/filter/LoggingFilter.kt
@@ -0,0 +1,56 @@
+package notbe.tmtm.ddanddanserver.presentation.filter
+
+import jakarta.servlet.FilterChain
+import jakarta.servlet.http.HttpServletRequest
+import jakarta.servlet.http.HttpServletResponse
+import org.springframework.stereotype.Component
+import org.springframework.web.filter.OncePerRequestFilter
+import org.springframework.web.util.ContentCachingRequestWrapper
+import org.springframework.web.util.ContentCachingResponseWrapper
+
+@Component
+class LoggingFilter : OncePerRequestFilter() {
+    override fun doFilterInternal(
+        request: HttpServletRequest,
+        response: HttpServletResponse,
+        filterChain: FilterChain,
+    ) {
+        val wrappedRequest = ContentCachingRequestWrapper(request)
+        val wrappedResponse = ContentCachingResponseWrapper(response)
+
+        logRequest(wrappedRequest)
+        filterChain.doFilter(wrappedRequest, wrappedResponse)
+        logResponse(wrappedResponse)
+
+        wrappedResponse.copyBodyToResponse()
+    }
+
+    private fun logRequest(request: ContentCachingRequestWrapper) {
+        val requestBody = String(request.contentAsByteArray, Charsets.UTF_8)
+
+        logger.info(
+            """
+            =========================== [REQUEST] ===========================
+            ▶ Method: ${request.method}
+            ▶ URI: ${request.requestURI}
+            ▶ Headers: ${request.headerNames.toList().joinToString { "$it: ${request.getHeader(it)}" }}
+            ▶ Body: $requestBody
+            =================================================================
+            """,
+        )
+    }
+
+    private fun logResponse(response: ContentCachingResponseWrapper) {
+        val responseBody = String(response.contentAsByteArray, Charsets.UTF_8)
+
+        logger.info(
+            """
+            =========================== [RESPONSE] ==========================
+            ▶ Status: ${response.status}
+            ▶ Headers: ${response.headerNames.joinToString { "$it: ${response.getHeader(it)}" }}
+            ▶ Body: $responseBody
+            =================================================================
+            """,
+        )
+    }
+}
