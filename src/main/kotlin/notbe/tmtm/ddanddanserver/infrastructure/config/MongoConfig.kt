054a948bb24b054311882b58d152dda23a1b1d2f
ddingmin <ddingmin.dev@gmail.com>
2025-04-02 17:58:16 +0900

    feat: mongoDB 시간 컨버터를 적용한다

--- src/main/kotlin/notbe/tmtm/ddanddanserver/infrastructure/config/MongoConfig.kt
+++ src/main/kotlin/notbe/tmtm/ddanddanserver/infrastructure/config/MongoConfig.kt
@@ -1,5 +1,9 @@
 package notbe.tmtm.ddanddanserver.infrastructure.config
 
+import notbe.tmtm.ddanddanserver.infrastructure.config.converter.DateToLocalDateConverter
+import notbe.tmtm.ddanddanserver.infrastructure.config.converter.DateToLocalDateTimeConverter
+import notbe.tmtm.ddanddanserver.infrastructure.config.converter.LocalDateTimeToDateConverter
+import notbe.tmtm.ddanddanserver.infrastructure.config.converter.LocalDateToDateConverter
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.data.mongodb.MongoDatabaseFactory
@@ -7,6 +11,7 @@ import org.springframework.data.mongodb.config.EnableMongoAuditing
 import org.springframework.data.mongodb.core.convert.DefaultDbRefResolver
 import org.springframework.data.mongodb.core.convert.DefaultMongoTypeMapper
 import org.springframework.data.mongodb.core.convert.MappingMongoConverter
+import org.springframework.data.mongodb.core.convert.MongoCustomConversions
 import org.springframework.data.mongodb.core.mapping.MongoMappingContext
 
 @Configuration
@@ -16,7 +21,21 @@ class MongoConfig {
     fun mappingMongoConverter(
         factory: MongoDatabaseFactory,
         context: MongoMappingContext,
+        kstLocalDateTimeWriterConverter: LocalDateTimeToDateConverter,
+        kstDateTimeReaderConverter: DateToLocalDateConverter,
+        kstLocalDateTimeReaderConverter: DateToLocalDateTimeConverter,
+        kstDateTimeWriterConverter: LocalDateToDateConverter,
     ) = MappingMongoConverter(DefaultDbRefResolver(factory), context).apply {
         setTypeMapper(DefaultMongoTypeMapper(null))
+        customConversions =
+            MongoCustomConversions(
+                listOf(
+                    kstLocalDateTimeWriterConverter,
+                    kstLocalDateTimeReaderConverter,
+                    kstDateTimeWriterConverter,
+                    kstDateTimeReaderConverter,
+                ),
+            )
+        afterPropertiesSet()
     }
 }
