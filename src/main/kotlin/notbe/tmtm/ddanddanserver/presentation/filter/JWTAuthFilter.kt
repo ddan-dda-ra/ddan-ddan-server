baf7752227b0c3ff18965666f612b0f9c9158380
ddingmin <ddingmin.dev@gmail.com>
2025-04-08 08:54:28 +0900

    refactor: common 패키지로 이동

--- src/main/kotlin/notbe/tmtm/ddanddanserver/presentation/filter/JWTAuthFilter.kt
+++ src/main/kotlin/notbe/tmtm/ddanddanserver/presentation/filter/JWTAuthFilter.kt
@@ -5,7 +5,7 @@ import jakarta.servlet.http.HttpServletRequest
 import jakarta.servlet.http.HttpServletResponse
 import notbe.tmtm.ddanddanserver.domain.exception.AuthenticationInvalidTokenException
 import notbe.tmtm.ddanddanserver.domain.exception.AuthenticationTokenNotExistException
-import notbe.tmtm.ddanddanserver.infrastructure.token.JWTTokenProvider
+import notbe.tmtm.ddanddanserver.common.JWTTokenProvider
 import org.springframework.security.core.context.SecurityContextHolder
 import org.springframework.web.filter.OncePerRequestFilter
 import org.springframework.web.servlet.HandlerExceptionResolver
