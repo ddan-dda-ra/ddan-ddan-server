d4cf74806181dbc937bcf2fed17183292467b489
ddingmin <ddingmin.dev@gmail.com>
2025-04-08 10:05:11 +0900

    feat: 잔존 파일 제거

--- src/main/kotlin/notbe/tmtm/ddanddanserver/domain/gateway/OAuthGateway.kt
+++
@@ -1,9 +0,0 @@
-package notbe.tmtm.ddanddanserver.domain.gateway
-
-import notbe.tmtm.ddanddanserver.domain.model.auth.OAuthInfo
-
-interface OAuthGateway {
-    fun getOAuthUserInfo(accessToken: String): OAuthInfo
-
-    fun getOAuthUserInfoFromApple(accessToken: String): OAuthInfo
-}
--- src/main/kotlin/notbe/tmtm/ddanddanserver/domain/model/auth/OAuthInfo.kt
+++
@@ -1,7 +0,0 @@
-package notbe.tmtm.ddanddanserver.domain.model.auth
-
-class OAuthInfo(
-    val id: String,
-    val type: OAuthType,
-    val nickName: String?,
-)
