b4521572020035c79d2549bb966d7d1b6f974928
ddingmin <ddingmin.dev@gmail.com>
2025-04-09 18:04:52 +0900

    refactor: 함수 분리

--- src/main/kotlin/notbe/tmtm/ddanddanserver/application/service/AuthService.kt
+++ src/main/kotlin/notbe/tmtm/ddanddanserver/application/service/AuthService.kt
@@ -6,6 +6,7 @@ import notbe.tmtm.ddanddanserver.domain.model.auth.Auth
 import notbe.tmtm.ddanddanserver.domain.model.auth.AuthResult
 import notbe.tmtm.ddanddanserver.domain.model.auth.OAuthType
 import notbe.tmtm.ddanddanserver.domain.model.user.User
+import notbe.tmtm.ddanddanserver.domain.usecase.auth.OAuth
 import notbe.tmtm.ddanddanserver.infrastructure.database.repository.AuthRepository
 import notbe.tmtm.ddanddanserver.infrastructure.database.repository.UserRepository
 import org.springframework.data.repository.findByIdOrNull
@@ -26,40 +27,11 @@ class AuthService(
         deviceToken: String?,
     ): AuthResult {
         val oAuth = oauthProcessorFactory.getClient(oAuthType).getOAuth(oAuthAccessToken)
-        val auth = authRepository.findByOAuthIdAndType(oAuth.id, oAuthType)
-
-        // 가입되지 않은 유저 CASE
-        if (auth == null) {
-            val newUser =
-                userRepository.save(
-                    User.register(name = oAuth.nickName, deviceToken = deviceToken),
-                )
-            authRepository.save(
-                Auth.create(oAuthId = oAuth.id, type = oAuthType, userId = newUser.id),
-            )
-            return AuthResult(
-                accessToken = jwtTokenProvider.createAccessToken(newUser),
-                refreshToken = jwtTokenProvider.createRefreshToken(newUser),
-                user = newUser,
-            )
+        authRepository.findByOAuthIdAndType(oAuth.id, oAuthType)?.let { auth ->
+            return loginExistUser(auth, deviceToken)
         }
 
-        // 가입된 유저 CASE
-        val user =
-            userRepository.findByIdOrNull(auth.userId)
-                ?: throw RuntimeException("auth와 user의 정합성 불일치")
-
-        // 디바이스 토큰 갱신
-        if (deviceToken != null && user.deviceToken != deviceToken) {
-            user.updateDeviceToken(deviceToken)
-            userRepository.save(user)
-        }
-
-        return AuthResult(
-            accessToken = jwtTokenProvider.createAccessToken(user),
-            refreshToken = jwtTokenProvider.createRefreshToken(user),
-            user = user,
-        )
+        return loginNewUser(oAuth, deviceToken, oAuthType)
     }
 
     @Transactional
@@ -73,5 +45,54 @@ class AuthService(
             refreshToken = jwtTokenProvider.createRefreshToken(user),
             user = user,
         )
+    }
+
+    private fun loginNewUser(
+        oAuth: OAuth,
+        deviceToken: String?,
+        oAuthType: OAuthType,
+    ): AuthResult {
+        // 가입되지 않은 유저 CASE
+        val newUser =
+            userRepository.save(
+                User.register(name = oAuth.nickName, deviceToken = deviceToken),
+            )
+        authRepository.save(
+            Auth.create(oAuthId = oAuth.id, type = oAuthType, userId = newUser.id),
+        )
+        return AuthResult(
+            accessToken = jwtTokenProvider.createAccessToken(newUser),
+            refreshToken = jwtTokenProvider.createRefreshToken(newUser),
+            user = newUser,
+        )
+    }
+
+    private fun loginExistUser(
+        auth: Auth,
+        deviceToken: String?,
+    ): AuthResult {
+        // 가입된 유저 CASE
+        val user =
+            userRepository.findByIdOrNull(auth.userId)
+                ?: throw RuntimeException("auth와 user의 정합성 불일치")
+
+        // 디바이스 토큰 갱신
+        deviceToken?.let { updateDeviceToken(it, user) }
+
+        return AuthResult(
+            accessToken = jwtTokenProvider.createAccessToken(user),
+            refreshToken = jwtTokenProvider.createRefreshToken(user),
+            user = user,
+        )
+    }
+
+    private fun updateDeviceToken(
+        deviceToken: String,
+        user: User,
+    ) {
+        if (user.deviceToken != deviceToken) {
+            user.updateDeviceToken(deviceToken)
+            userRepository.save(user)
+        }
     }
 }
