5ab49ebdeb95afefc87a5dc11bbd234206eee3b0
ddingmin <ddingmin.dev@gmail.com>
2025-04-20 12:34:08 +0900

    feat: 장난감 응답 개수와 장난감 실제 지급 로직 일원화

--- src/main/kotlin/notbe/tmtm/ddanddanserver/domain/usecase/user/UpdateCalorieAndRewardFood.kt
+++ src/main/kotlin/notbe/tmtm/ddanddanserver/domain/usecase/user/UpdateCalorieAndRewardFood.kt
@@ -46,8 +46,8 @@ class UpdateCalorieAndRewardFood(
 
         if (isDailyPurposeAchieve(calorieDailyInfo, user, input.calorie)) {
             calorieDailyInfo.purposeAchieved = true
-            user.toyQuantity++
             if (validateToyGiven(user.purposeStrict)) {
+                user.toyQuantity++
                 rewardedToyQuantity = 1
                 user.addPurposeStrict()
                 calorieDailyInfo.toyGiven = true
