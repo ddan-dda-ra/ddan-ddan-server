7d3f9b7749d22f094d1f43c7b9afaeef97ceac79
ddingmin <ddingmin.dev@gmail.com>
2025-04-20 12:41:16 +0900

    feat: 마이그레이션 코드 버전관리 (#157)
    * feat: 장난감 응답 개수와 장난감 실제 지급 로직 일원화
    
    * feat: 목표 마이그레이션 코드 버전관리
    
    * feat: 오늘 목표 달성 여부 로직 순서 변경

---
+++ src/test/kotlin/notbe/tmtm/ddanddanserver/migration/PurposeStrictMigration.kt
@@ -0,0 +1,45 @@
+package notbe.tmtm.ddanddanserver.migration
+
+import notbe.tmtm.ddanddanserver.infrastructure.database.repository.DailyInfoRepository
+import notbe.tmtm.ddanddanserver.infrastructure.database.repository.UserRepository
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import java.time.LocalDate
+
+@SpringBootTest
+class PurposeStrictMigration {
+    @Autowired
+    lateinit var userRepository: UserRepository
+
+    @Autowired
+    lateinit var dailyInfoRepository: DailyInfoRepository
+
+    @Test
+    fun migrate() {
+        val users = userRepository.findAll()
+        users.forEach { user ->
+
+            val date = LocalDate.now().minusDays(1)
+            var strict = 0L
+
+            while (true) {
+                val dailyInfo = dailyInfoRepository.findByUserIdAndDate(user.id, date.minusDays(strict))
+                if (dailyInfo == null || dailyInfo.purposeAchieved.not()) {
+                    dailyInfoRepository.findByUserIdAndDate(user.id, LocalDate.now())?.let {
+                        if (it.purposeAchieved) {
+                            strict += 1
+                        }
+                    }
+                    user.purposeStrict = strict.toInt()
+                    break
+                } else {
+                    strict += 1
+                }
+            }
+
+            println("userId: ${user.name}, strict: $strict")
+            userRepository.save(user)
+        }
+    }
+}
