5c86d97254d8e94cb2367aeb05c5a0ab4a057b84
ddingmin <ddingmin.dev@gmail.com>
2025-07-08 08:23:36 +0900

    #217 사용하지 않는 메서드 제거

--- src/main/kotlin/notbe/tmtm/ddanddanserver/infrastructure/database/repository/InviteCodeRepository.kt
+++ src/main/kotlin/notbe/tmtm/ddanddanserver/infrastructure/database/repository/InviteCodeRepository.kt
@@ -15,7 +15,3 @@ interface InviteCodeRepository : MongoRepository<InviteCode, ObjectId> {
 fun InviteCodeRepository.findByIdOrThrow(inviteCodeId: ObjectId): InviteCode =
     findByIdOrNull(inviteCodeId)
         ?: throw InviteCodeNotFoundException()
-
-fun InviteCodeRepository.findByCodeOrThrow(code: String): InviteCode =
-    findByCode(code)
-        ?: throw InviteCodeNotFoundException()
