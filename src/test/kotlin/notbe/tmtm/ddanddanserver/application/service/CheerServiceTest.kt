e20107fa5f5d8efbcf1cf5e8ee9babe8eb7786b8
ddingmin <ddingmin.dev@gmail.com>
2025-09-21 15:46:38 +0900

    feat: 응원하기 시 푸시 메시지를 발송한다

--- src/test/kotlin/notbe/tmtm/ddanddanserver/application/service/CheerServiceTest.kt
+++ src/test/kotlin/notbe/tmtm/ddanddanserver/application/service/CheerServiceTest.kt
@@ -9,27 +9,51 @@ import io.mockk.verify
 import notbe.tmtm.ddanddanserver.domain.exception.CheerAlreadyExistsException
 import notbe.tmtm.ddanddanserver.domain.exception.CheerNotFriendsException
 import notbe.tmtm.ddanddanserver.domain.model.cheer.Cheer
+import notbe.tmtm.ddanddanserver.domain.model.user.DeviceToken
+import notbe.tmtm.ddanddanserver.domain.model.user.User
+import notbe.tmtm.ddanddanserver.domain.model.user.UserSetting
+import notbe.tmtm.ddanddanserver.infrastructure.client.PushClient
 import notbe.tmtm.ddanddanserver.infrastructure.database.repository.CheerRepository
 import notbe.tmtm.ddanddanserver.infrastructure.database.repository.FriendshipRepository
+import notbe.tmtm.ddanddanserver.infrastructure.database.repository.UserRepository
 import notbe.tmtm.ddanddanserver.infrastructure.database.repository.areFriends
+import notbe.tmtm.ddanddanserver.infrastructure.database.repository.findByIdOrThrow
 import org.bson.types.ObjectId
 
 class CheerServiceTest : FunSpec({
     lateinit var cheerRepository: CheerRepository
     lateinit var friendshipRepository: FriendshipRepository
+    lateinit var userRepository: UserRepository
+    lateinit var pushClient: PushClient
     lateinit var cheerService: CheerService
 
     beforeEach {
         cheerRepository = mockk()
         friendshipRepository = mockk()
-        cheerService = CheerService(cheerRepository, friendshipRepository)
+        userRepository = mockk()
+        pushClient = mockk(relaxed = true)
+        cheerService = CheerService(cheerRepository, friendshipRepository, userRepository, pushClient)
     }
 
     test("친구 관계에서 응원을 생성할 수 있다") {
         val cheererId = ObjectId()
         val cheereeId = ObjectId()
         val expectedCheer = Cheer.create(cheererId, cheereeId)
+        val cheereeUser = User(
+            id = cheereeId,
+            deviceToken = DeviceToken("test-token"),
+            name = "테스트유저",
+            setting = UserSetting(isAppPushOn = true)
+        )
+        val cheererUser = User(
+            id = cheererId,
+            deviceToken = DeviceToken("cheerer-token"),
+            name = "응원자",
+            setting = UserSetting(isAppPushOn = true)
+        )
 
+        every { userRepository.findByIdOrThrow(cheereeId) } returns cheereeUser
+        every { userRepository.findByIdOrThrow(cheererId) } returns cheererUser
         every { friendshipRepository.areFriends(cheererId, cheereeId) } returns true
         every { cheerRepository.saveWithDuplicateCheck(any()) } returns expectedCheer
 
@@ -39,12 +63,27 @@ class CheerServiceTest : FunSpec({
         result.cheereeId shouldBe cheereeId
         verify { friendshipRepository.areFriends(cheererId, cheereeId) }
         verify { cheerRepository.saveWithDuplicateCheck(any()) }
+        verify { pushClient.sendToUser(any(), any(), any()) }
     }
 
     test("친구가 아닌 사용자에게는 응원할 수 없다") {
         val cheererId = ObjectId()
         val cheereeId = ObjectId()
+        val cheereeUser = User(
+            id = cheereeId,
+            deviceToken = DeviceToken("test-token"),
+            name = "테스트유저",
+            setting = UserSetting(isAppPushOn = true)
+        )
+        val cheererUser = User(
+            id = cheererId,
+            deviceToken = DeviceToken("cheerer-token"),
+            name = "응원자",
+            setting = UserSetting(isAppPushOn = true)
+        )
 
+        every { userRepository.findByIdOrThrow(cheereeId) } returns cheereeUser
+        every { userRepository.findByIdOrThrow(cheererId) } returns cheererUser
         every { friendshipRepository.areFriends(cheererId, cheereeId) } returns false
 
         shouldThrow<CheerNotFriendsException> {
@@ -57,7 +96,14 @@ class CheerServiceTest : FunSpec({
 
     test("자기 자신을 응원하려고 하면 예외가 발생한다") {
         val userId = ObjectId()
+        val user = User(
+            id = userId,
+            deviceToken = DeviceToken("test-token"),
+            name = "테스트유저",
+            setting = UserSetting(isAppPushOn = true)
+        )
 
+        every { userRepository.findByIdOrThrow(userId) } returns user
         every { friendshipRepository.areFriends(userId, userId) } returns false
 
         shouldThrow<CheerNotFriendsException> {
@@ -68,7 +114,21 @@ class CheerServiceTest : FunSpec({
     test("중복 응원 시도 시 예외가 발생한다") {
         val cheererId = ObjectId()
         val cheereeId = ObjectId()
+        val cheereeUser = User(
+            id = cheereeId,
+            deviceToken = DeviceToken("test-token"),
+            name = "테스트유저",
+            setting = UserSetting(isAppPushOn = true)
+        )
+        val cheererUser = User(
+            id = cheererId,
+            deviceToken = DeviceToken("cheerer-token"),
+            name = "응원자",
+            setting = UserSetting(isAppPushOn = true)
+        )
 
+        every { userRepository.findByIdOrThrow(cheereeId) } returns cheereeUser
+        every { userRepository.findByIdOrThrow(cheererId) } returns cheererUser
         every { friendshipRepository.areFriends(cheererId, cheereeId) } returns true
         every { cheerRepository.saveWithDuplicateCheck(any()) } throws CheerAlreadyExistsException()
 
